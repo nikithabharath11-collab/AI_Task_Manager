ed083479995ca6c2928caf3dea10faaf7069ddc0
nikithabharath11-collab <nikithabharath11@gmail.com>
2025-12-30 19:06:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,11 +106,6 @@ def logout():
     session.clear()
     return redirect("/")
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
-
 
 
 
